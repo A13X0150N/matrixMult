5e60d9f1aac53dcb2ce9b8a56dbf385f0e36890f
Alex Olson <alexolson87@gmail.com>
2019-08-04 01:58:24 -0700

    Added fma to experimental section

--- src/experimental/top_exp.sv
+++ src/experimental/top_exp.sv
@@ -1,21 +1,21 @@
 // top_exp.sv
 
 localparam CLOCK_PERIOD = 10;
-localparam CYCLES = 35;
+localparam CYCLES = 12;
 localparam FPBITS = 31;
 localparam x = 23;
 localparam y = 23;
-//localparam num1 = 1.0000002;  // man = 2   32'h3f800002
-//localparam num2 = 1.0000007;  // man = 6   32'h3f800006
-localparam num1 = 10.5;
-localparam num2 = 2.5;
+localparam num1 = 1.75;
+localparam num2 = 1.5;
+localparam num3 = 1.0;
 
 // Data types
 typedef enum bit [2:0] {
     IDLE,
-    LOAD1,
-    LOAD2,
+    LOAD,
     MULTIPLY,
+    ALIGN,
+    ACCUMULATE,
     NORMALIZE,
     OUTPUT,
     ERROR
@@ -29,14 +29,15 @@ typedef struct packed {
 
 typedef struct packed {
     bit sign;
-    bit [8:0]  exponent;
-    bit [23:0] mantissa;
+    bit [7:0]  exponent;
+    bit [49:0] mantissa;
 } internal_float_t;
 
 
 // Testbench
 module top_exp;
     initial $display("\n\n\t *** Starting Tests *** \n");
+    initial #(CLOCK_PERIOD * CYCLES) $finish;
 
     // Clock Generator
     bit clk;
@@ -51,7 +52,7 @@ module top_exp;
         rst = 0;
         @(posedge clk);
         rst = 1;
-        repeat (3) @(posedge clk);
+        @(posedge clk);
         rst = 0;
         @(posedge clk);
     endtask : reset
@@ -75,9 +76,7 @@ module top_exp;
     initial begin
         reset();
         start = 0;
-        $display("\n\t *** FMA test *** \n");
-        $monitor($time/CLOCK_PERIOD, " clock cycles\n\t float_in: %f   ready: %b   error: %b \n\t float_out: %f   float_out.exp: %d   float_out.man: %d \n\n",
-                    $bitstoshortreal(float_in), ready, error,  $bitstoshortreal(float_out), float_out.exponent, float_out.mantissa);
+        $display("\n * * * FMA test begin * * * \n");
         start = 1;
         float_in1 = $shortrealtobits(num1);
         float_in = float_in1;
@@ -88,22 +87,23 @@ module top_exp;
         float_in = float_in2;
         @(posedge clk);
 
-        calc_float = $shortrealtobits(num1*num2);
-        $display("float_in1: %f  %x    float_in2: %f  %x", $bitstoshortreal(float_in1), float_in1, $bitstoshortreal(float_in2), float_in2);
-        $display("float_in1.exp: %d   float_in1.man: %d   float_in2.exp: %d   float_in2.man: %d", float_in1.exponent, float_in1.mantissa, float_in2.exponent, float_in2.mantissa);
-        $display("\n*** PREDICTED VALUES ***\n\t calc_float: %f   calc_float.exp: %d   calc_float.man: %d \n\n", 
-                    $bitstoshortreal(calc_float), calc_float.exponent, calc_float.mantissa);
-        
-        float_in3 = $shortrealtobits(2.2);
+        float_in3 = $shortrealtobits(num3);
         float_in = float_in3;
         @(posedge clk);
 
+        calc_float = $shortrealtobits(num1 * num2 + num3);
+        $display("float_in1: %f  %x\t\t\t\tfloat_in2: %f  %x\t\t\t\tfloat_in3: %f  %x", $bitstoshortreal(float_in1), float_in1, $bitstoshortreal(float_in2), float_in2, $bitstoshortreal(float_in3), float_in3);
+        $display("float_in1.exp: %d   float_in1.man: %d\t\tfloat_in2.exp: %d   float_in2.man: %d\t\tfloat_in3.exp: %d   float_in3.man: %d", float_in1.exponent, float_in1.mantissa, $signed(float_in2.exponent), float_in2.mantissa, $signed(float_in3.exponent), float_in3.mantissa);
+        $display("\n ---- PREDICTED VALUES ----\ncalc_float: %f\n\tcalc_float.exp: %d\n\tcalc_float.man: %d \n\n", $bitstoshortreal(calc_float), calc_float.exponent, calc_float.mantissa);
+    
+        do begin
+            @(posedge clk);
+        end while (!ready);
+        $display("\n ---- OUTPUT VALUES ----\nfloat_out: %f\n\tfloat_out.exp: %d\n\tfloat_out.man: %d\n\n", $bitstoshortreal(float_out), float_out.exponent, float_out.mantissa);
+
     end
-    initial #(CLOCK_PERIOD * CYCLES) $finish;
     final $display("\n\n\t *** End of Tests *** \n");
 endmodule: top_exp
-
-
 
 
 // IEEE-754 floating point Fused Multiply Accumulate
@@ -112,173 +112,222 @@ module fma
     input           clk, rst, start,
     input  float_t  float_in,
     output float_t  float_out,
-    output logic    error,
-    output logic    ready
+    output bit      error,
+    output bit      ready
 );
 
-    float_t a, b, c;
-    //internal_float_t a, b, c;         // a * b + c = y
+    bit error_in;
+    internal_float_t a, b, c, y;         // a * b + c = y
     state_t state=IDLE, next_state;
-    logic [$clog2(x)-1:0] mult_counter;
-
-    logic [x+y:0] booth_var;
-    logic [x-1:0] sum;
-    logic [x+y-1:0] product;
 
     // Check for input errors (denormalized numbers, +infinity, -infinity, NaN)
-    assign error = (!float_in.exponent || float_in.exponent == 8'hFF) ? 1 : 0;
+    assign error_in = ((float_in && !float_in.exponent) || float_in.exponent == 8'hFF) ? 1 : 0;
 
     // State machine driver
     always_ff @(posedge clk) begin
         state <= rst ? IDLE : next_state;
-        //$strobe("%s", state);
-        //$strobe("float.exp: %d", float_out.exponent);
-        //$strobe($time/CLOCK_PERIOD, " clock cycles   booth_var: %b", booth_var);
-        //$strobe($time/CLOCK_PERIOD, " clock cycles     product:  %b \n", product);
+        $strobe(($time/CLOCK_PERIOD), " clock cycles \t%s", state);
     end
 
     // Next state logic
     always_comb begin
         unique case (state)
-            IDLE:       next_state = (!error && start) ? LOAD1 : start ? ERROR : IDLE;
-            LOAD1:      next_state = error ? ERROR : LOAD2;
-            LOAD2:      next_state = error ? ERROR : MULTIPLY;
-            MULTIPLY:   next_state = error ? ERROR : (mult_counter == (y - 1)) ? NORMALIZE : MULTIPLY;
-            NORMALIZE:  next_state = OUTPUT;
-            OUTPUT:     next_state = IDLE;
-            ERROR:      next_state = IDLE;
+            IDLE: begin
+                if (error_in) begin
+                    next_state <= ERROR;
+                end
+                else if (start) begin
+                    next_state <= LOAD;
+                end
+                else begin
+                    next_state <= IDLE;
+                end
+            end
+            LOAD: begin
+                if (error_in) begin
+                    next_state <= ERROR;
+                end
+                else begin
+                    next_state <= MULTIPLY;
+                end
+            end
+            MULTIPLY: begin
+                if (error_in) begin
+                    next_state <= ERROR;
+                end
+                else begin
+                    next_state <= ALIGN;
+                end
+            end
+            ALIGN: begin
+                if (!c.exponent && !c.mantissa) begin
+                    next_state <= OUTPUT;
+                end
+                else begin
+                    next_state <= ACCUMULATE;
+                end
+            end
+            ACCUMULATE: begin
+                next_state <= NORMALIZE;
+            end
+            NORMALIZE: begin
+                next_state <= OUTPUT;
+            end
+            OUTPUT: begin
+                if (($signed(float_out.exponent) > 127) || ($signed(float_out.exponent) < -128)) begin
+                    next_state <= ERROR;
+                end
+                else begin 
+                    next_state <= IDLE;
+                end
+            end
+            ERROR: begin 
+                next_state <= IDLE;
+            end
         endcase
     end
 
     // Clocked logic and I/O
     always_ff @(posedge clk) begin
         unique case (state)
-    
             IDLE: begin
-                mult_counter <= '0;
-                product <= '0;
                 ready <= 0;
                 if (start) begin
-                    booth_var <= {{x{1'b0}}, float_in.mantissa, 1'b0};
                     a.sign <= float_in.sign;
                     a.exponent <= float_in.exponent;
                     a.mantissa <= float_in.mantissa;
                 end
                 else begin
-                    booth_var <= '0;
                     a <= '0;
                 end
                 b <= '0;
                 c <= '0;
+                y <= '0;
                 float_out <= '0;
             end
-            
-            LOAD1: begin
-                mult_counter <= '0;
-                booth_var <= booth_var;
-                product <= '0;
+            LOAD: begin
                 ready <= 0;
                 a <= a;
                 b.sign <= float_in.sign;
                 b.exponent <= float_in.exponent;
                 b.mantissa <= float_in.mantissa;
                 c <= '0;
-                float_out <= float_out;
+                y <= '0;
+                float_out <= '0;
             end
-
-            LOAD2: begin
-                mult_counter <= '0;
-                booth_var <= booth_var;
-                product <= '0;
+            MULTIPLY: begin
                 ready <= 0;
                 a <= a;
                 b <= b;
                 c.sign <= float_in.sign;
                 c.exponent <= float_in.exponent;
                 c.mantissa <= float_in.mantissa;
-                float_out <= float_out;
-            end
-
-            MULTIPLY: begin
-                mult_counter <= mult_counter + 1'b1;
-                booth_var[x+y:x+1] <= {sum[x-1], sum[x-1:1]};
-                booth_var[x:0] <= {sum[0], booth_var[x:1]};
-                product <= product;
-                ready <= 0;
-                a <= a;
-                b <= b;
-                c <= c;
-                float_out.sign <= a.sign ^ b.sign;
-                float_out.exponent <= $signed(a.exponent) + $signed(b.exponent) - 127;
-                float_out.mantissa <= product;
-                //$strobe("float_out.exponent: %d", float_out.exponent);
-            end
-
-            NORMALIZE: begin
-                mult_counter <= mult_counter;
-                booth_var <= booth_var;
-                ready <= 0;
-                a <= a;
-                b <= b;
-                c <= c;
-                if (!booth_var[x+y]) begin
-                    $display("NORMALIZE!!!");
-                    product <= booth_var[x+y:1] << 1;
-                    float_out.sign <= float_out.sign;
-                    float_out.exponent <= $signed(float_out.exponent) - 1;
-                    float_out.mantissa <= float_out.mantissa;
+                if ((!a.exponent && !a.mantissa) || (!b.exponent && !b.mantissa)) begin
+                    y <= '0;
                 end
                 else begin
-                    product <= booth_var[x+y:1];
-                    float_out.sign <= float_out.sign;
-                    float_out.exponent <= float_out.exponent;
-                    float_out.mantissa <= float_out.mantissa;
+                    y.sign <= a.sign ^ b.sign;
+                    y.exponent <= ($signed(a.exponent)-127) + ($signed(b.exponent)-127) + 127;
+                    y.mantissa <= (a.mantissa | (1<<23)) * (b.mantissa | (1<<23));
                 end
+                float_out <= '0;
             end
-
+            ALIGN: begin
+                ready <= 0;
+                a <= a;
+                b <= b;
+                // If the product is denormalized
+                if (y.mantissa[47]) begin
+                    if (c.exponent < (y.exponent+1)) begin
+                        c.sign <= c.sign;
+                        c.exponent <= $signed(c.exponent) + ((y.exponent+1)-c.exponent);
+                        c.mantissa <= (c.mantissa | (1<<23)) >> ((y.exponent+1)-c.exponent);
+                        y.sign <= y.sign;
+                        y.exponent <= $signed(y.exponent) + 1;
+                        y.mantissa <= y.mantissa >> 1;
+                    end
+                    else begin
+                        c <= c;
+                        y.sign <= y.sign;
+                        y.exponent <= $signed(y.exponent) + 2;
+                        y.mantissa <= y.mantissa >> 2;
+                    end
+                end
+                // Else the product is normalized
+                else begin
+                    if (c.exponent < y.exponent) begin
+                        c.sign <= c.sign;
+                        c.exponent <= $signed(c.exponent) + (y.exponent-c.exponent);
+                        c.mantissa <= (c.mantissa | (1<<23)) >> (y.exponent-c.exponent);
+                        y <= y;
+                    end
+                    else begin
+                        c <= c;
+                        y.sign <= y.sign;
+                        y.exponent <= $signed(y.exponent) + (c.exponent-y.exponent);
+                        y.mantissa <= y.mantissa >> (c.exponent-y.exponent);
+                    end
+                end
+                float_out <= '0;
+            end
+            ACCUMULATE: begin
+                ready <= 0;
+                a <= a;
+                b <= b;
+                c <= c;
+                y.sign <= y.sign;
+                y.exponent <= y.exponent;
+                if (c.sign == y.sign) begin
+                    y.mantissa <= y.mantissa + (c.mantissa<<23);
+                end
+                else begin
+                    if (y.mantissa >= (c.mantissa<<23)) begin
+                        y.mantissa <= y.mantissa - (c.mantissa<<23);
+                    end
+                    else begin
+                        y.mantissa <= (c.mantissa<<23) - y.mantissa;
+                    end
+                end
+                float_out <= '0;
+            end
+            NORMALIZE: begin
+                ready <= 0;
+                a <= a;
+                b <= b;
+                c <= c;
+                if (y.mantissa[47]) begin                   
+                    y.sign <= y.sign;
+                    y.exponent <= $signed(y.exponent) + 1;
+                    y.mantissa <= y.mantissa >> 1;
+                end
+                else begin
+                    y <= y;
+                end
+                float_out <= '0;
+            end
             OUTPUT: begin
-                mult_counter <= mult_counter;
-                booth_var <= booth_var;
-                product <= product;
                 ready <= 1;
                 a <= a;
                 b <= b;
                 c <= c;
-                if ($signed(float_out.exponent) > 127) begin
-                    $display("!!! ERROR !!!: output exponent overflow");
+                y <= y;
+                if (($signed(float_out.exponent) > 127) || ($signed(float_out.exponent) < -128)) begin
                     float_out <= '1;
                 end
                 else begin
-                    $display("*** OUTPUT VALUES ***");
-                    float_out.sign <= float_out.sign;
-                    float_out.exponent <= float_out.exponent;
-                    float_out.mantissa <= product[x+y:23];   // ??????
+                    float_out.sign <= y.sign;
+                    float_out.exponent <= y.exponent;
+                    float_out.mantissa <= y.mantissa[45:23];
                 end
-
-                //$strobe("*** OUTPUT VALUES *** \n\t float_out: %f   float_out.exp: %d   float_out.man: %d \n\n", $bitstoshortreal(float_out), float_out.exponent, float_out.mantissa);
             end
-
             ERROR: begin
-                mult_counter <= '0;
-                booth_var <= '0;
-                product <= '0;
                 ready <= 0;
                 a <= '0;
                 b <= '0;
                 c <= '0;
+                y <= '0;
                 float_out <= '1;
             end
-
-        endcase
-    end
-
-    // Booth Encoding Logic 
-    always_comb begin
-        case (booth_var[1:0])
-            2'b00:  sum = booth_var[x+y:x+1];                       // No change
-            2'b01:  sum = booth_var[x+y:x+1] + b.mantissa;          // Add 
-            2'b10:  sum = booth_var[x+y:x+1] + (~b.mantissa + 1);   // Subtract (2's complement)
-            2'b11:  sum = booth_var[x+y:x+1];                       // No change
         endcase
     end
 
